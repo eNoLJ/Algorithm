55ba3632f55a4726d27fe55fa728921c856344cd
jeong-inho <derosatam76@gmail.com>
2021-01-13 11:09:12 +0900

    Complete LeetCode - Two Sum

--- BAEKJOON 1009/src/Main.java
+++ BAEKJOON 1009/src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int[][] baseArr = {{10}, {1}, {6, 2, 4, 8}, {1, 3, 9, 7}, {6, 4}, {5}, {6}, {1, 7, 9, 3}, {6, 8, 4, 2}, {1, 9}};
--- BAEKJOON 1076/src/Main.java
+++ BAEKJOON 1076/src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> colors = new ArrayList<>(Arrays.asList("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"));
 
