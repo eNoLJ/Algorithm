685781a4671ed49662c8008cce8084e371400e17
jeong-inho <derosatam76@gmail.com>
2021-09-02 17:34:58 +0900

    refactor: test 메소드의 위치 변경

--- Programmers/src/TheHighestAndLowestRanksOfTheLottery.java
+++ Programmers/src/TheHighestAndLowestRanksOfTheLottery.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheHighestAndLowestRanksOfTheLottery {
+
     public int[] solution(int[] lottos, int[] win_nums) {
         int zeroCount = (int) Arrays.stream(lottos).filter(lotto -> lotto == 0).count();
         int sameNumberCount = (int) Arrays.stream(lottos).filter(lotto -> checkNumber(lotto, win_nums)).count();
@@ -20,18 +21,13 @@ public class TheHighestAndLowestRanksOfTheLottery {
         int rank = Math.abs(7 - number);
         return rank < 7 ? rank : 6;
     }
-}
-
-class TheHighestAndLowestRanksOfTheLotteryTest {
 
     @Test
     public void test() {
-        TheHighestAndLowestRanksOfTheLottery problem = new TheHighestAndLowestRanksOfTheLottery();
         int[][] lottos = new int[][]{{44, 1, 0, 0, 31, 25}, {0, 0, 0, 0, 0, 0}, {45, 4, 35, 20, 3, 9}};
         int[][] win_nums = new int[][]{{31, 10, 45, 1, 6, 19}, {38, 19, 20, 40, 15, 25}, {20, 9, 3, 45, 4, 35}};
         int[][] expectedResult = new int[][]{{3, 5}, {1, 6}, {1, 1}};
-
         IntStream.range(0, expectedResult.length)
-                .forEach(i -> assertThat(expectedResult[i]).isEqualTo(problem.solution(lottos[i], win_nums[i])));
+                .forEach(i -> assertThat(expectedResult[i]).isEqualTo(solution(lottos[i], win_nums[i])));
     }
 }
