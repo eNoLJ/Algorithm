9c80a0d5d9920974fe6343a798e93a4afb444560
jeong-inho <derosatam76@gmail.com>
2021-09-19 22:05:42 +0900

    feat: 두 정수 사이의 합

--- HackerRank/src/DiagonalDifference.java
+++ HackerRank/src/DiagonalDifference.java
@@ -16,10 +16,10 @@ public class DiagonalDifference {
     }
 
     public static void main(String[] args) {
-        List<Integer> a = new ArrayList<>() { {add(11); add(2); add(4);} };
-        List<Integer> b = new ArrayList<>() { {add(4); add(5); add(6);} };
-        List<Integer> c = new ArrayList<>() { {add(10); add(8); add(-12);} };
-        List<List<Integer>> List = new ArrayList<>() { {add(a); add(b); add(c);} };
+        List<Integer> a = new ArrayList<Integer>() { {add(11); add(2); add(4);} };
+        List<Integer> b = new ArrayList<Integer>() { {add(4); add(5); add(6);} };
+        List<Integer> c = new ArrayList<Integer>() { {add(10); add(8); add(-12);} };
+        List<List<Integer>> List = new ArrayList<List<Integer>>() { {add(a); add(b); add(c);} };
 
         System.out.println(diagonalDifference(List)); // 15
     }
