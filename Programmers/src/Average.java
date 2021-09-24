9e16a3f8bbc85c884ee46018ee854a75f0c9dbc9
jeong-inho <derosatam76@gmail.com>
2021-09-24 15:15:53 +0900

    feat: 평균 구하기

---
+++ Programmers/src/Average.java
@@ -0,0 +1,19 @@
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Average {
+
+    public double solution(int[] arr) {
+        return (double) Arrays.stream(arr).sum() / arr.length;
+    }
+
+    @Test
+    public void test() {
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(solution(new int[]{1, 2, 3, 4})).isEqualTo(2.5);
+        softly.assertThat(solution(new int[]{5, 5})).isEqualTo(5);
+        softly.assertAll();
+    }
+}
