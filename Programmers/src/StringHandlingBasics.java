abec2bb952ad9b9cbc26a07f704aafcb9bb5a6b2
jeong-inho <derosatam76@gmail.com>
2021-10-01 23:21:00 +0900

    feat: 문자열 다루기 기본

---
+++ Programmers/src/StringHandlingBasics.java
@@ -0,0 +1,29 @@
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+public class StringHandlingBasics {
+
+    public boolean solution(String s) {
+        int strLength = s.length();
+
+        if (strLength != 4 && strLength != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < strLength; i++) {
+            if (s.charAt(i) > '9') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Test
+    public void test() {
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(solution("a234")).isFalse();
+        softly.assertThat(solution("1234")).isTrue();
+        softly.assertAll();
+    }
+}
